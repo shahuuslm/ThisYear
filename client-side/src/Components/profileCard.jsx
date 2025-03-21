7c3de28044208629d2dff7721baad19a10bef23a
shahuuslm <mdshahriyarslm@gmail.com>
2025-03-21 15:23:38 +0600

    first commit

---
+++ client-side/src/Components/profileCard.jsx
@@ -0,0 +1,10 @@
+
+const profileCard = () => {
+    return (
+        <div>
+            <h1>iecub</h1>
+        </div>
+    );
+};
+
+export default profileCard;
---
+++ client-side/src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Home.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
