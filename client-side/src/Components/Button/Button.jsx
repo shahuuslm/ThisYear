f63690f02471c780be35f449b539e266f21360d8
shahuuslm <mdshahriyarslm@gmail.com>
2025-03-21 19:20:41 +0600

    implemented hover on button

--- client-side/src/Components/Button/Button.jsx
+++ client-side/src/Components/Button/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = ({text}) => {
     return (
         <div>
-            <button className="font-medium text-amber-100 bg-amber-950 px-2 py-1 rounded-sm w-full ">
+            <button className="font-medium text-amber-100 bg-amber-950 px-2 py-1 rounded-sm w-full transition ease-in-out hover:scale-90 hover:cursor-pointer ">
                 <p>{text}</p>
             </button>
         </div>
